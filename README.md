3c2596f731814c5e50316a3992d6036964a9dd66
Dexter Zhang <dexter_zhang@trendmicro.com>
2022-03-07 20:12:17 +1100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# photography
+# Welcome to your CDK TypeScript project - PhotographyStack
+
+This is a blank project for TypeScript development with CDK.
+
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
+## Useful commands
+
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `cdk deploy` deploy this stack to your default AWS account/region
+- `cdk diff` compare deployed stack with current state
+- `cdk synth` emits the synthesized CloudFormation template
+
+You are a photographer and want to keep your photos in a place that you can view when you're away from your computer.
+
+we provision an environment via CDK that includes a CloudFront Distrubution backed by an S3 Object Store. We
+use these resources to serve our static assets, including our index.html and images. To make it easier to identify the
+resources we created via our CloudFormation stack we added [CfnOutput](https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.CfnOutput.html).
+
+[CDK CloudFront API Docs](https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_cloudfront-readme.html)
+to see if there were ways we could server the index.html without specifying it in the URL.
+
+## CRUD APIS 
+
+A website that you manually have to update each time to add a new image isn't fun. In starting out CRUD APIs we
+simplified how we upload images and have images displayed out our website.
+
+Time permitting, we were able to add monitoring to help us gain visibility on the availability of our website.
+
+
+## CDN validation:
+
+```
+aws cloudformation describe-stack-resources --stack-name PhotographyStack --query "StackResources[?starts_with(LogicalResourceId,'photographydistribution') && ResourceType=='AWS::CloudFront::Distribution'].PhysicalResourceId" --output text
+```
+
+```
+aws cloudfront create-invalidation --distribution-id ****** --paths "/*"
+```
+
+## upload files to s3
+
+```
+aws s3 cp src/web/js/app.js s3://BUCKET/js/app.js
+
+aws s3 cp src/web/index.html s3://BUCKET
+
+aws s3 cp src/web/styles/main.css s3://BUCKET/styles/main.css
+```
---
+++ lib/photography-stack.ts
@@ -0,0 +1,157 @@
+import { Stack, StackProps, CfnOutput, Duration } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { S3EventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from "aws-cdk-lib/aws-events-targets";
+
+export class PhotographyStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here 
+
+    // example resource 
+    const appBucket = new s3.Bucket(this, "photography-app-bucket", {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL
+    });
+
+    // S3 bucket for photos 
+    const photoBucket = new s3.Bucket(this, "photography-photo-bucket", {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL
+    });
+
+    const appDistribution = new cloudfront.Distribution(this, 'photography-distribution', {
+      defaultBehavior: { origin: new origins.S3Origin(appBucket) },
+      defaultRootObject: "index.html",
+      additionalBehaviors: {
+        '/photos/*': {
+          origin: new origins.S3Origin(photoBucket)
+        }
+      }
+    });
+
+
+    const createFunction = new lambda.Function(this, "photography-upload-function", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("src/create"),
+      handler: "index.handler",
+      environment: {
+        "BUCKET": photoBucket.bucketName
+      }
+    })
+
+    photoBucket.grantWrite(createFunction)
+
+    const photoApi = new apigateway.RestApi(this, "photography-photo-api", {
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS
+      }
+    })
+
+    const uploadResource = photoApi.root.addResource("photo");
+    uploadResource.addMethod("POST", new apigateway.LambdaIntegration(createFunction))
+
+
+    // DDB table store image data 
+    const photoTable = new dynamodb.Table(this, "photography-table", {
+      partitionKey: {
+        name: "id", type: dynamodb.AttributeType.STRING
+      }
+    })
+
+    const registerFunction = new lambda.Function(this, "photography-function-register", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('src/register'),
+      handler: 'index.handler',
+      environment: {
+        "TABLE_NAME": photoTable.tableName
+      }
+    })
+
+    registerFunction.addEventSource(new S3EventSource(photoBucket, {
+      events: [
+        s3.EventType.OBJECT_CREATED
+      ]
+    }))
+
+    photoBucket.grantRead(registerFunction)
+    photoTable.grantWriteData(registerFunction)
+
+    // List Images Lambda 
+    const listFunction = new lambda.Function(this, "photography-function-list", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('src/list'),
+      handler: 'index.handler',
+      environment: {
+        "TABLE_NAME": photoTable.tableName
+      }
+    })
+
+    photoTable.grantReadData(listFunction)
+
+    uploadResource.addMethod("GET", new apigateway.LambdaIntegration(listFunction))
+
+    // delete Images Lambda 
+    const deleteFunction = new lambda.Function(this, "photography-function-delete", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('src/delete'),
+      handler: 'index.handler',
+      environment: {
+        "TABLE_NAME": photoTable.tableName,
+        "BUCKET": photoBucket.bucketName
+      }
+    })
+    // add delete function permissions to delete photo and ddb register 
+    photoBucket.grantReadWrite(deleteFunction)
+    photoBucket.grantDelete(deleteFunction)
+    photoTable.grantWriteData(deleteFunction)
+
+    uploadResource.addMethod("DELETE", new apigateway.LambdaIntegration(deleteFunction))
+    uploadResource.addMethod("PUT", new apigateway.LambdaIntegration(deleteFunction))
+
+    // SNS Topic 
+    const monitorTopic = new sns.Topic(this, "WebsiteIsDownTopic", {
+      topicName: "WebsiteIsDown",
+      displayName: "Website is Down"
+    })
+
+    // Lambda function 
+    const monitorWebsiteFunction = new lambda.Function(this, "MonitorWebsite", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("src/monitor"),
+      handler: "index.handler",
+      environment: {
+        "SNS_TOPIC": monitorTopic.topicArn
+      }
+    })
+
+    // create Cloudwatch rule 
+    new events.Rule(this, 'MonitorWebsiteRule', {
+      description: "CloudWatch event to trigger Lambda every one minute",
+      schedule: events.Schedule.rate(Duration.minutes(1)),
+      targets: [
+        // add CloudWatch target 
+        new targets.LambdaFunction(monitorWebsiteFunction, {
+          event: events.RuleTargetInput.fromObject({})
+        })
+      ]
+    });
+
+    new CfnOutput(this, "AppBucket", { value: appBucket.bucketName })
+    new CfnOutput(this, "CfDistDomainName", { value: appDistribution.domainName })
+    new CfnOutput(this, "CfDistDomainId", { value: appDistribution.distributionId })
+
+    new CfnOutput(this, "PhotoBucket", { value: photoBucket.bucketName })
+    new CfnOutput(this, "APIEndpoint", { value: photoApi.url })
+    new CfnOutput(this, "UploadResource", { value: uploadResource.path })
+
+  }
+}
